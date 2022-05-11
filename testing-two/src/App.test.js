d19e0f636f7e21930a7465446884293c05c89e82
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-11 10:32:46 -0400

    import extend-expect from testing library

--- testing-two/src/App.test.js
+++ testing-two/src/App.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 //import { Provider } from 'react-redux';
 //import { store } from './app/store';
 import App from './components/App';
-import {screen} from '@testing-library/dom'
+//import {screen} from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect';
+
 
 test('renders learn react link', () => {
   render(
-    //<Provider store={store}>
+    //<Provider> //store={store}>
       <App />
     //</Provider>
   );
 
   expect(screen.getByText('This has been more work than it is worth!')).toBeInTheDocument();
 });
+
